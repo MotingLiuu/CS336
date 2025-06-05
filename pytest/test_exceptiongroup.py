e06ecc68d84f49c866e98e3c1ce6a82b6b5fdd18
MotingLiuu <moting.liuu@gmail.com>
2025-06-05 12:28:15 +0900

    updated pytest examples

--- pytest/test_exceptiongroup.py
+++ pytest/test_exceptiongroup.py
@@ -8,8 +8,17 @@ def f():
             ],
     )
 
+def g():
+    raise Exception
+
+def test_exception():
+    with pytest.raises(Exception) as excinfo:
+        g()
+    assert isinstance(excinfo.value, Exception)
+
 def test_exception_in_group():
     with pytest.raises(ExceptionGroup) as excinfo:
         f()
     assert excinfo.group_contains(RuntimeError)
     assert not excinfo.group_contains(TypeError)
+
